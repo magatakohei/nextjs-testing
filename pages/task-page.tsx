9d26a7cd330750fc7ca3f259c4fbe5b20af629e9
magako <qutianh@gmail.com>
2021-03-06 20:13:10 +0900

    Error msg changed

--- pages/task-page.tsx
+++ pages/task-page.tsx
@@ -1,10 +1,48 @@
 import Layout from '../components/Layout'
+import { GetStaticProps } from 'next'
+import { getAllTasksData } from '../lib/fetch'
+import useSWR from 'swr'
+import axios from 'axios'
+import { TASK } from '../types/Types'
 
-const TaskPage: React.VFC = () => {
+interface STATICPROPS {
+  staticTasks: TASK[]
+}
+
+const axiosFetcher = async () => {
+  const result = await axios.get<TASK[]>(
+    'https://jsonplaceholder.typicode.com/todos/?_limit=10'
+  )
+  return result.data
+}
+
+const TaskPage: React.VFC<STATICPROPS> = ({ staticTasks }) => {
+  const { data: tasks, error } = useSWR('todosFetch', axiosFetcher, {
+    initialData: staticTasks,
+    revalidateOnMount: true,
+  })
+  if (error) return <span>Eror!</span>
   return (
     <Layout title="Todos">
       <p className="text-4xl">todos page</p>
+      <ul>
+        {tasks &&
+          tasks.map((task) => (
+            <li key={task.id}>
+              {task.id}
+              {': '}
+              <span>{task.title}</span>
+            </li>
+          ))}
+      </ul>
     </Layout>
   )
 }
 export default TaskPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const staticTasks = await getAllTasksData()
+  return {
+    props: { staticTasks },
+  }
+}
